b7f86ec39b871e50a3fe84fa0a671e0c4d144d16
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-22 19:27:46 -0500

    Create basic fetch function

---
+++ marta-interface/src/pages/LinesPage.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from react;
+
+const API_URL = "https://midsem-bootcamp-api.onrender.com/";
+
+function LinesPages() {
+
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    async function fetchData() {
+        try {
+            const response = await fetch(API_URL + "arrivals/gold");
+            if (!response.ok) {
+                throw Error("Problem in fetching data");
+            }
+            const newData = await response.json();
+            setData(newData);
+            setError(null);
+        } catch (error) {
+            console.log("Error occured: " + error);
+            setError(error);
+        }
+    }
+}
+
+export default LinesPages;
