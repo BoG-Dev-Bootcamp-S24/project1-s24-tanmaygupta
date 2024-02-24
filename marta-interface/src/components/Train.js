611138f0bf7bcce3bc490d893e10af84c2b2ab27
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-24 04:01:27 -0500

    Create basic Train component

--- marta-interface/src/components/Train.js
+++ marta-interface/src/components/Train.js
@@ -1,3 +1,33 @@
-export default function Train( {} ) {
+export default function Train( {train} ) {
+    const color = train.LINE
+    let border = "";
+    let background = "";
+    if (color === "GOLD") {
+        border = "border-y-yellow-500"
+        background = "bg-yellow-500"
+    } else if (color === "BLUE") {
+        border = "border-y-blue-500"
+        background = "bg-blue-500"
+    } else if (color === "GREEN") {
+        border = "border-y-green-500"
+        background = "bg-green-500"
+    } else if (color === "RED") {
+        border = "border-y-red-500"
+        background = "bg-red-500"
+    }
 
+    return (
+
+        <div className={`flex justify-start py-8 ${border} border-2`}>
+            <p className="ml-4 w-10 text-5xl"> M </p>
+            <div className="flex flex-col w-96 ml-16 items-start">
+                <p> {train.STATION} --{'>'} {train.DESTINATION} </p>
+                <div className="flex flex-row">
+                    <div className="w-16 mr-10"> <p className={`text-white ${background}`}> {train.LINE} </p> </div>
+                    {train.DELAY_TIME === "T0S" ? <p className="text-green-500"> On Time </p> : <p className="text-red-500"> Delayed </p>}
+                </div>
+            </div>
+            <p className="flex items-center"> {train.WAITING_TIME} </p>
+        </div>
+    )
 }
--- marta-interface/src/pages/LinesPage.js
+++ marta-interface/src/pages/LinesPage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Train from '../components/Train';
 
 const API_URL = "https://midsem-bootcamp-api.onrender.com/";
 
@@ -7,16 +8,18 @@ function LinesPages() {
 
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     async function fetchData() {
         try {
-            const response = await fetch(API_URL + "arrivals/blue");
+            const response = await fetch(API_URL + "arrivals/red");
             if (!response.ok) {
                 throw Error("Problem in fetching data");
             }
             const newData = await response.json();
             setData(newData);
             setError(null);
+            setLoading(false);
         } catch (error) {
             console.log("Error occured: " + error);
             setError(error);
@@ -27,10 +30,44 @@ function LinesPages() {
         fetchData()
     }, [])
 
+    function displayTrains(trains) {
+        if (trains.length === 0) {
+            return (
+                <div className='flex flex-col justify-start items-center p-10'>
+                    <h2 className='font-bold text-xl'> There are currently no trains running at this moment. </h2>
+                    <br />
+                    <h2 className='text-xl'>Please check back again later</h2>
+                </div>
+            )
+        } else {
+            const lastIndexMap = {};
+
+            trains.forEach((train, index) => {
+                const key = train.DESTINATION + train.STATION
+                lastIndexMap[key] = index;
+            })
+            const filterTrains = (trains.filter((train, index) => { //filter to only include latest data
+                const key = train.DESTINATION + train.STATION
+                return lastIndexMap[key] === index
+            }))
+            return (
+                <div>
+                    <p> {filterTrains.length} </p>
+                    {
+                        filterTrains.map((train) => {
+                            return ( <Train train={train} /> ) //map to print all unique trains
+                        })
+                    }
+                </div>
+            )
+        }
+    }
+
     return (
         <div className="lines-page">
             { error && <p> Error occurred </p>}
-            { data && <p> Data fetched properly </p>}
+            { loading && <h1 className='text-2xl font-bold flex p-10'> Loading... </h1>}
+            { data && displayTrains(data) }
         </div>
     )
 }
