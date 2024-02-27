c706c8e9fd5c64e0b82109a9972b2d5a239ac33e
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-27 18:34:18 -0500

    Migrate functionality to TrainsList.js

---
+++ marta-interface/src/components/TrainsList.js
@@ -0,0 +1,37 @@
+import Train from "./Train";
+
+export default function TrainsList( {trainsList} ) {
+
+    const trains = trainsList
+
+    if (trains.length === 0) {
+        return (
+            <div className='flex flex-col justify-start items-center p-10'>
+                <h2 className='font-bold text-xl'> There are currently no trains running at this moment. </h2>
+                <br />
+                <h2 className='text-xl'>Please check back again later</h2>
+            </div>
+        )
+    } else {
+        const lastIndexMap = {};
+
+        trains.forEach((train, index) => {
+            const key = train.DESTINATION + train.STATION
+            lastIndexMap[key] = index;
+        })
+        const filterTrains = (trains.filter((train, index) => { //filter to only include latest data
+            const key = train.DESTINATION + train.STATION
+            return lastIndexMap[key] === index
+        }))
+        return (
+            <div>
+                <p> {filterTrains.length} </p>
+                {
+                    filterTrains.map((train) => {
+                        return ( <Train train={train} /> ) //map to print all unique trains
+                    })
+                }
+            </div>
+        )
+    }
+}
--- marta-interface/src/pages/LinesPage.js
+++ marta-interface/src/pages/LinesPage.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import Train from '../components/Train';
+import TrainsList from '../components/TrainsList';
 
 const API_URL = "https://midsem-bootcamp-api.onrender.com/";
 
 function LinesPages() {
-
 
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
@@ -12,7 +12,7 @@ function LinesPages() {
 
     async function fetchData() {
         try {
-            const response = await fetch(API_URL + "arrivals/red");
+            const response = await fetch(API_URL + "arrivals/gold");
             if (!response.ok) {
                 throw Error("Problem in fetching data");
             }
@@ -23,6 +23,7 @@ function LinesPages() {
         } catch (error) {
             console.log("Error occured: " + error);
             setError(error);
+            setLoading(false);
         }
     }
 
@@ -30,44 +31,11 @@ function LinesPages() {
         fetchData()
     }, [])
 
-    function displayTrains(trains) {
-        if (trains.length === 0) {
-            return (
-                <div className='flex flex-col justify-start items-center p-10'>
-                    <h2 className='font-bold text-xl'> There are currently no trains running at this moment. </h2>
-                    <br />
-                    <h2 className='text-xl'>Please check back again later</h2>
-                </div>
-            )
-        } else {
-            const lastIndexMap = {};
-
-            trains.forEach((train, index) => {
-                const key = train.DESTINATION + train.STATION
-                lastIndexMap[key] = index;
-            })
-            const filterTrains = (trains.filter((train, index) => { //filter to only include latest data
-                const key = train.DESTINATION + train.STATION
-                return lastIndexMap[key] === index
-            }))
-            return (
-                <div>
-                    <p> {filterTrains.length} </p>
-                    {
-                        filterTrains.map((train) => {
-                            return ( <Train train={train} /> ) //map to print all unique trains
-                        })
-                    }
-                </div>
-            )
-        }
-    }
-
     return (
         <div className="lines-page">
             { error && <p> Error occurred </p>}
             { loading && <h1 className='text-2xl font-bold flex p-10'> Loading... </h1>}
-            { data && displayTrains(data) }
+            { data && <TrainsList trainsList={data} />}
         </div>
     )
 }
