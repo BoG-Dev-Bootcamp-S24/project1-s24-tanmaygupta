99d0b118a19c486e8d3114df11b07147db87cd4d
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-29 23:41:18 -0500

    Decode stations for minor API mismatch

--- marta-interface/src/components/Stations.js
+++ marta-interface/src/components/Stations.js
@@ -18,8 +18,18 @@ export default function Stations( {stations, setCurrStation, currStation} ) {
         <div className="text-white bg-slate-800 flex flex-col text-lg">
             <button className={currStation === null ? "text-lg w-100 px-10 py-3 bg-slate-700" : "text-lg px-10 py-3"} onClick={() => clicked(null)}> All Stations </button>
             {stations.map((station) => {
+                let checkStation = station
+                if (station === "Hamilton E. Holmes") {
+                    checkStation = "HE Holmes"
+                } else if (station === "GWCC/CNN Center") {
+                    checkStation = "Dome"
+                } else if (station === "Edgewood") {
+                    checkStation = "Candler Park"
+                } else if (station === "Lakewood/Ft. McPherson") {
+                    checkStation = "Lakewood"
+                }
                 return (
-                    <button className={currStation === station ? "text-lg w-100 px-10 py-3 bg-slate-700" : "text-lg px-10 py-3"} onClick={() => clicked(station)}> {station} </button>
+                    <button className={currStation === checkStation ? "text-lg w-100 px-10 py-3 bg-slate-700" : "text-lg px-10 py-3"} onClick={() => clicked(station)}> {station} </button>
                 )
             })}
         </div>
